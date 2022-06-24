e9dad48ef1686dbfd5623f4a2c9d3f46bbc857ee
Noelle K <noeykuti@gmail.com>
2022-06-24 15:28:25 -0400

    filled controller with CRUD functions that return dummy responses res.send()

---
+++ controllers/catsController.js
@@ -0,0 +1,16 @@
+
+const addCat = (req, res) => {
+    res.send('Add a cat!');
+}
+
+const editCat = (req, res) => {
+    res.send('Edit a cat!');
+}
+
+const deleteCat = (req, res) => {
+    res.send('Delete a cat!');
+}
+
+const viewCats = (req, res) => {
+    res.send('View cats!')
+}
