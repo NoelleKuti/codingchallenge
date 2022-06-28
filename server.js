0b2b35a6e7ca3a503d34baa49a4919dd25f84d93
Noelle K <noeykuti@gmail.com>
2022-06-28 19:32:56 -0400

    changed app.get to try to fix heroku issue

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('Connected To Express!');
 })
 
